aaf83ae529f4c935c253e6521017acff97159980
Takaya-Murokawa <takaya-murokawa@dac.co.jp>
2020-06-05 02:52:01 +0000

    user registration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,8 +21,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
-
+    // public const HOME = '/home';
+    
+    public const HOME = '/';
+      
     /**
      * Define your route model bindings, pattern filters, etc.
      *
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,5 +14,14 @@
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
+        
+        
+        <ul class="nav navbar-nav navbar-right">
+            {{-- ユーザ登録ページへのリンク --}}
+            <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+            {{-- ログインページへのリンク --}}
+            <li><a href="#">Login</a></li>
+        </ul>
+        
     </nav>
 </header>
